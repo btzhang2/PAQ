8b97e3545325c5c2cc193ecee284308c4e0648c5
Piotr Milewski <pmilewski@stuy.edu>
2017-04-21 05:33:29 -0400

    Initialized HelpDesk

---
+++ HelpDesk.java
@@ -0,0 +1,11 @@
+public class HelpDesk{
+
+    //instance variables
+    private ArrayPriorityQueue<Tickets> ArrayPQ;
+
+    public HelpDesk{
+	ArrayPQ = new ArrayPriorityQueue<Tickets>();
+    }
+}
+
+    
