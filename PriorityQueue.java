9b01c741476e8042b69f181e42a1734b23bd61a2
Brooke Jin <bjin@stuy.edu>
2017-04-21 11:25:39 -0400

    added interface

---
+++ PriorityQueue.java
@@ -0,0 +1,11 @@
+public interface PriorityQueue {
+
+    public void add(Comparable x);
+
+    public boolean isEmpty();
+
+    public Comparable peekMin();
+
+    public Comparable removeMin();
+
+}
