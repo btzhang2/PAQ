a1634574444eacefecc0a7387aa8af631009c877
piotrmilewski <pmilewski@stuy.edu>
2017-04-23 18:17:52 -0400

    Initial Ticket

--- Ticket.java
+++ Ticket.java
@@ -1,20 +1,51 @@
-//Ticket
-import cs1.Keyboard;
-
-public class Ticket{
+public class Ticket implements Comparable<Ticket>{
     
     //instance vars
     private int id;
     private int VIPLevel;
-    private String problem;
     private boolean isSolved;
     private String name;
+    private String problem;
     private String solution;
 
-    public Ticket(){
-	System.out.println("How important is your request?");
-	int inputVIPLevel = Keyboard.readInt();
-	VIPLevel = inputVIPLevel;
+    public Ticket(int Tid, int TVIPLevel, String Tname, String Tproblem){
+	id = Tid;
+	VIPLevel = TVIPLevel;
+	name = Tname;
+	problem = Tproblem;
     }
 
+    public int getVIPLevel(){
+	return VIPLevel;
+    }
+
+    public int getStatus(){
+	return isSolved;
+    }
+
+    public int setVIPLevel(int newVIPLevel){
+	VIPLevel = newVIPLevel;
+    }
+    
+    public void setStatus(boolean status){
+	isSolved = status;
+    }
+
+    public void setSolution(String Tsolution){
+	solution = Tsolution;
+    }
+
+    public int compareTo(Ticket referenceName){
+	return VIPLevel - referenceName.getVIPLevel();
+    }
+
+    public String toString(){
+	String retStr = "Ticket Number: " + id;
+	retStr += "\nName: " + name;
+	retStr += "\nVIPLevel: " + VIPLevel;
+	retStr += "\nStatus: " + isSolved;
+	retStr += "\nProblem: " + problem;
+	retStr += "\nSolution: " + solution;
+	return retStr;
+    }	
 }
